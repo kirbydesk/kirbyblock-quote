28baf0e8c4475886181033dff403b6915ea6532a
Christian Fickinger <chrfickinger@users.noreply.github.com>
2025-10-16 10:31:37 +0200

    Plugin structure

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<?php
+<?php Kirby::plugin('chrfickinger/kirbyblock-quote', [
 
-	// Show error if "kirby-fields" is not installed
-	if (!Kirby::plugin('chrfickinger/kirby-fields')) {
-		die('Plugin "kirby-fields" required! Please install it first.');
-	}
-
-	Kirby::plugin('chrfickinger/kirbyblock-quote', [
+	/* -------------- Extensions --------------*/
+	'translations' => require_once 'src/extensions/translations.php',
 
 ]);
---
+++ src/i18n/de.php
@@ -0,0 +1,4 @@
+<?php return array(
+
+	'kirbyblockquote.title' => 'Zitat',
+);
