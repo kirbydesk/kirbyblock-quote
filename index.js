d63d8335dfcf2a3188bbc64ec45d987b0466dfe0
Christian Fickinger <mail@christianfickinger.de>
2026-03-17 10:37:36 +0100

    Add new theme variant

--- index.js
+++ index.js
@@ -171,14 +171,15 @@
             vars["--pw-color-block-background"] = this.content.backgroundcolor;
           }
           const btnStyle = this.content.buttonstyle || "default";
-          if (btnStyle === "variant" && this.colors.variant) {
-            const btnKeys = Object.keys(this.colors.variant).filter((k) => k.startsWith("pw-color-button"));
+          if (btnStyle !== "default" && this.colors[btnStyle]) {
+            const btnKeys = Object.keys(this.colors[btnStyle]).filter((k) => k.startsWith("pw-color-button"));
             for (const key of btnKeys) {
-              vars["--" + key] = this.colors.variant[key];
+              vars["--" + key] = this.colors[btnStyle][key];
             }
           }
         } else {
-          const palette = style === "variant" ? { ...this.colors.default, ...this.colors.variant } : this.colors.default;
+          const themePalette = this.colors[style];
+          const palette = themePalette ? { ...this.colors.default, ...themePalette } : this.colors.default;
           for (const [key, value] of Object.entries(palette)) {
             vars["--" + key] = value;
           }
